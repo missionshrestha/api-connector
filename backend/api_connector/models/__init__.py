a4071261489dd0115efee88efc5a3e1da52b2f85
Mission Shrestha <missionshrestha99@gmail.com>
2026-06-18 16:04:59 +0545

    Refactor model string representations and update imports in migration files

--- backend/api_connector/models/__init__.py
+++ backend/api_connector/models/__init__.py
@@ -1,4 +1,8 @@
 # backend/api_connector/models/__init__.py
+from api_connector.models.auth_config import AuthConfig
+from api_connector.models.connection_profile import ConnectionProfile
+from api_connector.models.connection_test_result import ConnectionTestResult
+from api_connector.models.endpoint import Endpoint
 from api_connector.models.enums import (
     ArrayHandling,
     AuthType,
@@ -6,23 +10,19 @@ from api_connector.models.enums import (
     InferredType,
     PaginationStrategy,
 )
-from api_connector.models.connection_profile import ConnectionProfile
-from api_connector.models.auth_config import AuthConfig
-from api_connector.models.endpoint import Endpoint
 from api_connector.models.pagination_config import PaginationConfig
 from api_connector.models.schema_field import SchemaField
-from api_connector.models.connection_test_result import ConnectionTestResult
 
 __all__ = [
-    "AuthType",
-    "PaginationStrategy",
-    "InferredType",
     "ArrayHandling",
-    "HTTPMethod",
-    "ConnectionProfile",
     "AuthConfig",
-    "Endpoint",
-    "PaginationConfig",
-    "SchemaField",
+    "AuthType",
+    "ConnectionProfile",
     "ConnectionTestResult",
+    "Endpoint",
+    "HTTPMethod",
+    "InferredType",
+    "PaginationConfig",
+    "PaginationStrategy",
+    "SchemaField",
 ]
